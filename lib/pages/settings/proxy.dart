37b62c43b0debc70c1e45f4914fd705d0ddcfbd3
Dave <dave.turmawan@outlook.com>
2024-01-27 10:37:01 +0000

    To do

--- lib/pages/settings/proxy.dart
+++ lib/pages/settings/proxy.dart
@@ -1,1 +1,3 @@
-//Make a checkbox
+//Make a checkbox for Automatically Detect System Settings
+//Make a checkbox for Enable Proxy Server
+//Make a dropdown for Proxy Server Type (HTTP, HTTPS, SOCKS4, SOCKS5)
